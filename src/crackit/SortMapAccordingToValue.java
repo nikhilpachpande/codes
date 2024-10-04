3e40015e7afe3375546dbbb1d50da4e6d57b7935
nikhil.pachapande <nikhil.pachapande@P2CLAP-2L1C103.dir.svc.accenture.com>
2024-10-04 15:24:54 +0530

    sort map according to value

--- src/crackit/SortMapAccordingToValue.java
+++ src/crackit/SortMapAccordingToValue.java
@@ -1,5 +1,36 @@
 package crackit;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
 public class SortMapAccordingToValue {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Map<String , Integer> map = new HashMap<>();
+		
+		   map.put("a", 3);
+	        map.put("b", 1);
+	        map.put("c", 2);
+		
+	        Map<String , Integer> map1 =
+	        		
+	        		map.entrySet()
+	        		.stream()
+	        		.sorted(Entry.comparingByValue(Integer::compare))
+	        		.collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(e1, e2) -> e1,LinkedHashMap::new  ));
+	        
+	        //(e1, e2) -> e1 this tells in case of duplicate keys choose one value;
+		
+		System.out.println(map1);
+		
+		
+		
+	}
 
 }
