40c436b19b9627dcb1e5f66a7ddc3249474b5763
nikhil.pachapande <nikhil.pachapande@P2CLAP-2L1C103.dir.svc.accenture.com>
2024-10-04 11:27:45 +0530

    no use just for fun

--- src/crackit/ZTest.java
+++ src/crackit/ZTest.java
@@ -1,13 +1,18 @@
 package crackit;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringJoiner;
 
-//find the occurance of non repeating character
+//format date
 
 
 
@@ -16,20 +21,16 @@ public class ZTest {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(fibonachi(5));
-	}
-	
-	
-
+		
+		LocalDateTime dt = LocalDateTime.now();
+		String np = dt.format(DateTimeFormatter.ofPattern("dd"));
+		 
+		 System.out.println(np);
 		
 		
-
-	
-	public static int fibonachi(int n) {
-		if(n==0 ) return 0;
-		if(n==1 || n==2) return 1;
 		
-		return fibonachi(n-1)+ fibonachi(n-2);
+		 
+		
 	}
 	
 	
